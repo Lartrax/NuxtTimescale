0f8f54272b8a1c31cb140681db49f0a59b0ebac3
Lars Erik Nordbø <lartrax909@gmail.com>
2023-02-05 17:15:28 +0100

    update colors for better darkmode

--- app.vue
+++ app.vue
@@ -1,13 +1,14 @@
 <template>
   <div
-    class="w-full h-screen flex justify-center items-center pt-40 bg-white dark:invert"
+    class="w-full h-screen flex justify-center items-center py-80 bg-white dark:invert"
+    :class="darkmode"
   >
     <div class="w-4/5 flex flex-col items-center gap-8">
       <div class="flex flex-col gap-2 w-full sm:flex-row">
         <label>https://go-api-postgres-7xqyrnpuaq-ew.a.run.app/v1/ </label>
         <select
           v-model="selected"
-          class="border-black border hover:bg-orange-50"
+          class="border-black border hover:bg-orange-100"
         >
           <option>get</option>
           <option>add</option>
@@ -19,7 +20,7 @@
         <!--Selection for column-->
         <select
           v-model="command"
-          class="border-black border hover:bg-orange-50"
+          class="border-black border hover:bg-orange-100"
         >
           <option></option>
           <option>first_name</option>
@@ -42,7 +43,7 @@
           <input
             v-model="dataToSend"
             v-on:keyup.enter="getData()"
-            class="border-black border pl-1 w-full hover:bg-orange-50"
+            class="border-black border pl-1 w-full hover:bg-orange-100"
           />
         </div>
         <div>
@@ -50,7 +51,7 @@
           <input
             v-model="dataToSend2"
             v-on:keyup.enter="getData()"
-            class="border-black border pl-1 w-full hover:bg-orange-50"
+            class="border-black border pl-1 w-full hover:bg-orange-100"
           />
         </div>
       </div>
@@ -65,7 +66,7 @@
           id="input"
           v-model="dataToSend"
           v-on:keyup.enter="getData()"
-          class="border-black border pl-1 w-full hover:bg-orange-50"
+          class="border-black border pl-1 w-full hover:bg-orange-100"
         />
       </div>
       <!--Buttons-->
@@ -89,13 +90,15 @@
           </button>
         </div>
       </div>
+      <!--Darkmode-->
+      <button @click="darkmodeBool = !darkmodeBool">Darkmode</button>
       <!--Big table-->
       <div id="table" class="overflow-y-scroll w-full max-h-[50vh]">
         <div class="bg-white p-2 shadow-inner">
           <div
             v-for="(item, id) in responseData"
             :key="id"
-            class="p-2 shadow mb-2 odd:bg-orange-100 even:bg-white"
+            class="p-2 shadow mb-2 odd:bg-orange-200 even:bg-white"
           >
             <p v-if="item.first_name">first_name : {{ item.first_name }}</p>
             <p v-if="item.id">
@@ -110,7 +113,7 @@
               </i>
               <button
                 @click="insert(item.id)"
-                class="text-xs shadow border border-gray-500 rounded-full ml-2 px-2 py-0.5 active:text-neutral-400 hover:bg-orange-50 hover:border-gray-300"
+                class="text-xs shadow border border-gray-500 rounded-full ml-2 px-2 py-0.5 active:text-neutral-400 hover:bg-orange-100 hover:border-gray-300"
               >
                 insert
               </button>
@@ -158,10 +161,18 @@
           </div>
         </div>
       </div>
-      <p v-if="status == 'Success'" class="text-green-400 dark:invert">
+      <p
+        v-if="status == 'Success'"
+        class="text-green-400 dark:invert"
+        :class="darkmode"
+      >
         {{ status }}
       </p>
-      <p v-if="status == 'Failed'" class="text-red-400 dark:invert">
+      <p
+        v-if="status == 'Failed'"
+        class="text-red-400 dark:invert"
+        :class="darkmode"
+      >
         {{ status }}
       </p>
       <button
@@ -179,6 +190,7 @@
 export default {
   data() {
     return {
+      darkmodeBool: false,
       status: "",
       showEmpty: false,
       showOnlyIdName: false,
@@ -190,10 +202,18 @@ export default {
       timer: 0,
       buttonTitle: "Query",
       buttonStyle:
-        "border-black hover:rounded-xl hover:scale-105 hover:bg-orange-100 active:bg-orange-200",
+        "border-black hover:rounded-xl hover:scale-105 hover:bg-orange-200 active:bg-orange-300",
     };
   },
-  computed: {},
+  computed: {
+    darkmode() {
+      if (this.darkmodeBool) {
+        return "invert";
+      } else {
+        return "";
+      }
+    },
+  },
   methods: {
     async copyText(mytext) {
       await navigator.clipboard.writeText(mytext);
@@ -215,7 +235,7 @@ export default {
         }
         this.buttonTitle = "Query";
         this.buttonStyle =
-          "border-black hover:rounded-xl hover:scale-105 hover:bg-orange-100 active:bg-orange-200";
+          "border-black hover:rounded-xl hover:scale-105 hover:bg-orange-200 active:bg-orange-300";
       }
     },
     getData() {
